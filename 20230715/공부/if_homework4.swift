50fd13fc222f410a9755911334d480ed517122f1
KangJiHun1028 <minsakr10@naver.com>
2023-07-15 21:03:53 +0900

    homework

---
+++ 20230715/공부/if_homework4.swift
@@ -0,0 +1,21 @@
+let a = 10;
+
+if (a > 10) {
+    if (a - 5 > 1) {
+        print("출력 A")
+    }
+}
+else {
+    if (a == 10)
+    {
+        if (a > 5)
+        {
+            print("출력 E")
+        }
+    }
+    else {
+        print("출력D")
+    }
+    
+    print("출력B")
+}
